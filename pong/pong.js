a49f296bf0da74bbda5342374fb136d5925831aa
Dan Larsson <dan.jv.larsson@gmail.com>
2014-11-22 15:41:21 +0100

    new pong

--- pong/pong.js
+++ pong/pong.js
@@ -8,8 +8,13 @@ canvas.width = width;
 canvas.height = height;
 var context = canvas.getContext('2d');
 var player = new Player();
-var computer = new Computer();
+var player2 = new Player2();
 var ball = new Ball(200, 300);
+
+
+var multiplayer = null;
+var updateBallX = null;
+var updateBallY = null;
 
 var keysDown = {};
 
@@ -17,14 +22,14 @@ var render = function () {
     context.fillStyle = "#FFA500";
     context.fillRect(0, 0, width, height);
     player.render();
-    computer.render();
+    player2.render();
     ball.render();
 };
 
 var update = function () {
     player.update();
-    computer.update(ball);
-    ball.update(player.paddle, computer.paddle);
+    player2.update();
+    ball.update(player.paddle, player2.paddle);
 };
 
 var step = function () {
@@ -61,29 +66,26 @@ Paddle.prototype.move = function (x, y) {
     }
 };
 
-function Computer() {
+function Player2() {
     this.paddle = new Paddle(175, 10, 50, 10);
 }
 
-Computer.prototype.render = function () {
+Player2.prototype.render = function () {
     this.paddle.render();
 };
 
-Computer.prototype.update = function (ball) {
-    var x_pos = ball.x;
-    var diff = -((this.paddle.x + (this.paddle.width / 2)) - x_pos);
-    if (diff < 0 && diff < -4) {
-        diff = -5;
-    } else if (diff > 0 && diff > 4) {
-        diff = 5;
-    }
-    this.paddle.move(diff, 0);
-    if (this.paddle.x < 0) {
-        this.paddle.x = 0;
-    } else if (this.paddle.x + this.paddle.width > 400) {
-        this.paddle.x = 400 - this.paddle.width;
-    }
-};
+Player2.prototype.update = function () {
+//     for (var key in keysDown) {
+//         var value = Number(key);
+//         if (value == 38) {
+//             this.paddle.move(-4, 0);
+//         } else if (value == 40) {
+//             this.paddle.move(4, 0);
+//         } else {
+//             this.paddle.move(0, 0);
+//         }
+//     }
+// };
 
 function Player() {
     this.paddle = new Paddle(175, 580, 50, 10);
@@ -121,42 +123,50 @@ Ball.prototype.render = function () {
 };
 
 Ball.prototype.update = function (paddle1, paddle2) {
-    this.x += this.x_speed;
-    this.y += this.y_speed;
-    var top_x = this.x - 5;
-    var top_y = this.y - 5;
-    var bottom_x = this.x + 5;
-    var bottom_y = this.y + 5;
 
-    if (this.x - 5 < 0) {
-        this.x = 5;
-        this.x_speed = -this.x_speed;
-    } else if (this.x + 5 > 400) {
-        this.x = 395;
-        this.x_speed = -this.x_speed;
-    }
+    if (!multiplayer) {
+        this.x += this.x_speed;
+        this.y += this.y_speed;
+        var top_x = this.x - 5;
+        var top_y = this.y - 5;
+        var bottom_x = this.x + 5;
+        var bottom_y = this.y + 5;
 
-    if (this.y < 0 || this.y > 600) {
-        this.x_speed = 0;
-        this.y_speed = 3;
-        this.x = 200;
-        this.y = 300;
-    }
-
-    if (top_y > 300) {
-        if (top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
-            this.y_speed = -3;
-            this.x_speed += (paddle1.x_speed / 2);
-            this.y += this.y_speed;
+        if (this.x - 5 < 0) {
+            this.x = 5;
+            this.x_speed = -this.x_speed;
+        } else if (this.x + 5 > 400) {
+            this.x = 395;
+            this.x_speed = -this.x_speed;
         }
-    } else {
-        if (top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
+
+        if (this.y < 0 || this.y > 600) {
+            this.x_speed = 0;
             this.y_speed = 3;
-            this.x_speed += (paddle2.x_speed / 2);
-            this.y += this.y_speed;
+            this.x = 200;
+            this.y = 300;
+        }
+
+        if (top_y > 300) {
+            if (top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
+                this.y_speed = -3;
+                this.x_speed += (paddle1.x_speed / 2);
+                this.y += this.y_speed;
+            }
+        } else {
+            if (top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
+                this.y_speed = 3;
+                this.x_speed += (paddle2.x_speed / 2);
+                this.y += this.y_speed;
+            }
         }
     }
-};
+    else {
+        this.x = updateBallX;
+        this.y = updateBallY;
+    }
+});
+
 
 document.body.appendChild(canvas);
 animate(step);
@@ -167,7 +177,12 @@ window.addEventListener("keydown", function (event) {
 
 window.addEventListener("keyup", function (event) {
     delete keysDown[event.keyCode];
-});
+
+
+
+
+
+
 
 //controlercode from box2d
 
@@ -279,6 +294,22 @@ window.addEventListener('load', function() {
 
         });
 
+    }).catch(
+    function(message) {
+      // Device Orientation Events are not supported
+        Player.prototype.update = function () {
+    for (var key in keysDown) {
+        var value = Number(key);
+        if (value == 37) {
+            this.paddle.move(-4, 0);
+        } else if (value == 39) {
+            this.paddle.move(4, 0);
+        } else {
+            this.paddle.move(0, 0);
+        }
+    }
+};
+      // Implement manual fallback controls instead...
     });
 
 }, false);
@@ -306,3 +337,5 @@ window.addEventListener('load', function() {
 //    initialBeta = null;
 
 //}, false);
+
+});
